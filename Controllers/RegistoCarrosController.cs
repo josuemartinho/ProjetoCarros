728b209b160aef505bf9624dfedac0cb51b54c30
josuemartinho <josuem4rtinho@gmail.com>
2023-10-19 16:36:13 +0100

    Adicionar arquivos de projeto.

---
+++ Controllers/RegistoCarrosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoCarros.Controllers
+{
+    public class RegistoCarrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/RegistoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjetoCarros.Models
+{
+    public class RegistoModel
+    {
+        public int Id { get; set; }
+        public required string Marca { get; set; }
+        public required string Modelo { get; set; }
+        public required int Preco { get; set; }
+        public required int AnoCarro { get; set; }
+        public required string TipoCombustivel { get; set; }
+        public required string Cor { get; set; }
+        public DateTime UltimaAtualização { get; set; } = DateTime.Now;
+    }
+
+}
