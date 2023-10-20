94edc85c58620d94a135f7afd124f19d9543140f
Josue Martinho <josuem4rtinho@gmail.com>
2023-10-20 12:50:00 +0100

    -

--- Controllers/RegistoCarrosController.cs
+++ Controllers/RegistoCarrosController.cs
@@ -1,12 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoCarros.Data;
+using ProjetoCarros.Models;
 
 namespace ProjetoCarros.Controllers
 {
     public class RegistoCarrosController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+		readonly private ApplicationDBContext _db;
+		public RegistoCarrosController(ApplicationDBContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<RegistoModel> registos = _db.RegistosCarros;
+			return View(registos);
+		}
+		[HttpGet]
+		public IActionResult NovoRegisto()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult NovoRegisto(RegistoModel registos)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.RegistosCarros.Add(registos);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+
+			}
+			return View();
+		}
+	}
 }
+
